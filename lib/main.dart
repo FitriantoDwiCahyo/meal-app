212f7bd926a1cc200c84e6681ac6a449447c1687
riandwicahyo <riandwicahyo28@gmail.com>
2022-11-04 21:20:44 +0700

    modify file main.dart and categories screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../screens/meal_detail_screen.dart';
 import '../screens/categories_screen.dart';
 import '../screens/category_meals_screen.dart';
+import '../screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,7 +47,7 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/': (ctx) => const CategoriesScreen(),
+          '/': (ctx) => TabsScreen(),
           CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
           MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
         },
