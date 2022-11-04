7528f1ea1f18cb8e8d3afc010b4ac0818643aa93
riandwicahyo <riandwicahyo28@gmail.com>
2022-11-04 21:19:57 +0700

    add tabs_screen file to choose screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import './favorites_screen.dart';
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Meal'),
+          bottom: TabBar(
+            dragStartBehavior: DragStartBehavior.start,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(children: <Widget> [
+          CategoriesScreen(),
+          FavoritesScreen(),
+        ],),
+      ),
+    );
+  }
+}
